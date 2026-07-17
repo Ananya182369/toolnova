46c81d2d2bd6adaa3ec74ac6678d20756785ef3e
Ananya182369 <ananyalal143@gmail.com>
2026-07-17 11:34:30 +0530

    Implement search feature for card filtering
    Added search functionality to filter cards based on user input.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("ToolNova Loaded 🚀");
+const searchInput = document.getElementById("searchInput");
+const cards = document.querySelectorAll(".card");
+
+searchInput.addEventListener("keyup", function () {
+    const value = this.value.toLowerCase();
+
+    cards.forEach(card => {
+        const text = card.innerText.toLowerCase();
+
+        if(text.includes(value)){
+            card.style.display = "block";
+        }else{
+            card.style.display = "none";
+        }
+    });
+});
